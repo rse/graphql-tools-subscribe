32362ae7cee37a9da0653c5d04524abf698caf55
Ralf S. Engelschall <rse@engelschall.com>
2018-05-06 14:25:31 +0200

    finish API documentation

--- src/gts.d.ts
+++ src/gts.d.ts
@@ -58,13 +58,82 @@ declare module "graphql-tools-subscribe" {
               `server: GraphQLIOServer`)  */
         public at(hookName: string, handler: (...args: any[]) => any): () => void
 
-        /*  Open the processing.
-            This instanciates the internal network connections.  */
+        /*  Open the service.
+            This instanciates the internal pubsub/keyval mechanism.  */
         public open(): Promise<GTS>
 
         /*  Close the processing.
-            This drops the internal network connections.  */
+            This drops the internal pubsub/keyva mechanism.  */
         public close(): Promise<GTS>
+
+        /*  Create a logical connection, identified by the Connection Id (CID) **cid**, and
+            provide callback to get notified of outdated Subscription Ids (SIDs) **sids**.
+            The **cid** usually is a unique identier of the underlying network connection.
+            In the **notify** callback, the application usually notifies the peer over the
+            underlying network connection about the outdated subscriptions (which usually
+            in turn refetches the corresponding GraphQL queries).  */
+        public connection(cid: string, notify: (sids: string[]) => void): Connection
+
+        /*  Create a new internal scope without a corresponding connection and record a change.
+            Use this for application internal (non-client triggered) GraphQL mutations.
+            The parameters are the same as for `Scope#record()`. */
+        public scopeRecord(type: string, oid: string, action: string, via: string, onto: string): void
+
+        /*  Generate the GraphQL schema entries and resolver functions  */
+        public schemaSubscription (): string
+        public resolverSubscription (): Resolver
+        public schemaSubscribe (): string
+        public resolverSubscribe (): Resolver
+        public schemaUnsubscribe (): string
+        public resolverUnsubscribe (): Resolver
+        public schemaSubscriptions (): string
+        public resolverSubscriptions (): Resolver
+        public schemaPause (): string
+        public resolverPause (): Resolver
+        public schemaResume (): string
+        public resolverResume (): Resolver
+
+        /*  Dump a textual description of the current connection/subscription/record information  */
+        public dump(): string
+    }
+
+    /*  a GraphQL resolver  */
+    type Resolver = (obj: any, args: any, ctx: any, info: any) => any
+
+    /*  The logical connection  */
+    class Connection {
+        /*  Create a new tracking scope and attach it to the connection.
+            Notice: it is required that you pass-through this scope to all
+            GraphQL resolvers in the context object under field `scope`!  */
+        public scope (query: string, variables?: object): Scope
+
+        /*  Destroy connection (and all its attached scopes)  */
+        public destroy (): void
+    }
+
+    /*  The tracking scope  */
+    class Scope {
+        /*  Record a data mode access.
+            The **type** is a domain-specific type of the object the access happened onto.
+            The **oid** is a domain-specific identifier of the object the access happended onto.
+            The **action** is one of the CRUD operations: `read`, `create`, `update` or `delete`.
+            The **via** is the way the object was approached: `direct` or via a `relation`.
+            The **onto** is the way the object was approached: `one`, `many` or `all`.
+            Not all combinations are valid. The valid combinations for `action:via:onto` are:
+                - read:(direct|relation):(one|many|all)
+                - create:direct:one
+                - update:direct:(one|many|all)
+                - delete:direct:(one|many|all)  */
+        public record(type: string, oid: string, action: string, via: string, onto: string): void
+
+        /*  Commit scope  */
+        public commit (): void
+
+        /*  Reject scope  */
+        public reject (): void
+
+        /*  Destroy scope  */
+        public destroy (): void
     }
 
     const gts: GTS
